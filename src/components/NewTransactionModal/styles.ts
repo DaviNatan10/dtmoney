c44e0f6f0cf9b4257c280bd8c39999a0b00eea2c
DaviNatan10 <davinatanjprn1@outlook.com>
2025-03-27 14:36:59 -0300

    style; colors button

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.div `
   h2 {
@@ -56,13 +56,21 @@ gap: 0.5rem;
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#e52e4D',
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
-  background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+  background: ${(props) => props.isActive 
+  ? transparentize(0.9, colors[props.activeColor])
+   : 'transparent'};
   display: flex;
   align-items: center;
   justify-content: center;
